f0c708458dacde3e40c1235a92375c4e45731cbe
saguywalker <guyguy252@gmail.com>
2018-11-05 15:40:42 +0700

    add proof of work algorithm

--- main.go
+++ main.go
@@ -3,30 +3,44 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
 
 func main() {
 	bc := NewBlockchain()
-	bc.AddBlock("Block 1 from skywalker.")
-	for i := 0; i < len(bc.blocks); i++ {
-		fmt.Println(hex.EncodeToString(bc.blocks[i].Hash), ":", string(bc.blocks[i].Data))
-	}
-
+	bc.AddBlock("Test 1st block!")
+	fmt.Println(bc.blocks[1].Hash)
+	fmt.Println(bc.blocks[1].Data)
+	fmt.Println(bc.blocks[1].Nonce)
+	fmt.Println(bc.blocks[1].Timestamp)
 }
+
+const targetBits = 24
+const maxNonce = math.MaxInt64
 
 type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int64
 }
 
 type Blockchain struct {
 	blocks []*Block
+}
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func IntToHex(num int64) []byte {
+	return []byte(strconv.FormatInt(num, 16))
 }
 
 func (b *Block) setHash() {
@@ -37,9 +51,12 @@ func (b *Block) setHash() {
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.setHash()
-	return block
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Solve()
+	block.Nonce = int64(nonce)
+	block.Hash = hash
+	return pow.block
 }
 
 func (bc *Blockchain) AddBlock(data string) {
@@ -54,4 +71,48 @@ func NewGenesisBlock() *Block {
 
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(3)
+	target.Lsh(target, uint(256-targetBits))
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Solve() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	fmt.Printf("Mining the block \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Println("\n")
+
+	return nonce, hash[:]
 }
