fcd797e619286b0d15b096e4788a868f2b04a191
saguywalker <guyguy252@gmail.com>
2018-11-06 14:44:18 +0700

    add blockchain iterator struct

--- main.go
+++ main.go
@@ -41,6 +41,11 @@ type ProofOfWork struct {
 	target *big.Int
 }
 
+type BlockchainIterator struct {
+	currentBlockHash []byte
+	db               *bolt.DB
+}
+
 func IntToHex(num int64) []byte {
 	return []byte(strconv.FormatInt(num, 16))
 }
@@ -182,4 +187,23 @@ func DeserializeBlock(d []byte) *Block {
 		fmt.Printf("Error to bytes: %s", d)
 	}
 	return &block
+}
+
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	return &BlockchainIterator{bc.tip, bc.db}
+}
+
+func (it *BlockchainIterator) Next() *Block {
+	var block *Block
+	err := it.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		blockHash := b.Get(it.currentBlockHash)
+		block = DeserializeBlock(blockHash)
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error with bolt db file", err)
+	}
+
+	return block
 }
